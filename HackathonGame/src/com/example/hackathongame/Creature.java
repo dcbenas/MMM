40ffc906fb3fa6ba73ab6dbd3ca94e7f3834e553
hchen95 <hchen95@emory.edu>
2014-04-12 13:29:05 -0400

    added creature interface

---
+++ HackathonGame/src/com/example/hackathongame/Creature.java
@@ -0,0 +1,11 @@
+package com.example.hackathongame;
+
+public interface Creature {
+	String getName();
+	String getStats();
+	int getRarity();
+	String getImageID();
+	void setName(String name);
+	void setRarity(int rarity);
+	void setImageID(String imgID);
+}
