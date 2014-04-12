32a5a60c30a0571c24253be66747d1ee5d39f768
dcbenas <dcbenas@gmail.com>
2014-04-12 12:45:29 -0400

    psuedologin screen complete, along with welcome splash.

--- HackathonGame/src/com/example/hackathongame/MainActivity.java
+++ HackathonGame/src/com/example/hackathongame/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.hackathongame;
 
 import android.os.Bundle;
+
 import android.app.Activity;
+import android.content.Intent;
+
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	private EditText username, password;
+	private String uid, pass;
+	private Button doLogin;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		username = (EditText)findViewById(R.id.login_username);
+		password = (EditText)findViewById(R.id.login_password);
+		
+		doLogin = (Button)findViewById(R.id.login_submit);
+		doLogin.setOnClickListener(loginListener);
+
 	}
+	private OnClickListener loginListener = new OnClickListener(){
+		public void onClick(View v){
+			if (username.getText().toString().equals("admin") && password.getText().toString().equals("password")){
+				Intent loggedIn = new Intent(getBaseContext(), MapScreen.class);
+				loggedIn.putExtra("username", username.getText().toString());
+				startActivity(loggedIn);
+			} else {
+				Toast.makeText(getApplicationContext(), "dumbass", Toast.LENGTH_LONG).show();
+			}
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
---
+++ HackathonGame/src/com/example/hackathongame/MapScreen.java
@@ -0,0 +1,28 @@
+package com.example.hackathongame;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MapScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_screen);
+		Bundle previousVars = getIntent().getExtras();
+		String username = previousVars.getString("username");
+		
+		TextView user = (TextView) findViewById(R.id.userField);
+		user.setText(username);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map_screen, menu);
+		return true;
+	}
+
+}
