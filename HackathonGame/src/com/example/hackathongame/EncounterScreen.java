d13632d54767accb85d3ee6561f77df562988e17
spzheng <stephanie.p.zheng@gmail.com>
2014-04-13 12:07:47 -0400

    monster pictures

--- HackathonGame/src/com/example/hackathongame/EncounterScreen.java
+++ HackathonGame/src/com/example/hackathongame/EncounterScreen.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
@@ -15,6 +16,7 @@ import java.util.Random;
 public class EncounterScreen extends Activity {
 	TextView adjective1, adjective2, rarityView, hpView, adView, defView, name;
     Random rand;
+    ImageView image;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +64,7 @@ public class EncounterScreen extends Activity {
         adView = (TextView) findViewById(R.id.encounter_ad_display);
         defView = (TextView) findViewById(R.id.encounter_def_display);
         name = (TextView) findViewById(R.id.encounter_creature_type);
+        image = (ImageView) findViewById(R.id.imageView1);
         
         adjective1.setText(Generator.generateAdj());
         adjective2.setText(Generator.generateAdj());
@@ -75,24 +78,28 @@ public class EncounterScreen extends Activity {
 				adView.setText("5");
 				defView.setText("1");
 				rarityView.setText("Pleb Tier");
+				image.setImageResource(R.drawable.creature1);
 				break;
 			case 2:
 				hpView.setText("60");
 				adView.setText("10");
 				defView.setText("2");
 				rarityView.setText("Uncommon");
+				image.setImageResource(R.drawable.creature2);
 				break;
 			case 3:
 				hpView.setText("90");
 				adView.setText("15");
 				defView.setText("3");
 				rarityView.setText("RARE");
+				image.setImageResource(R.drawable.creature3);
 				break;
 			case 4:
 				hpView.setText("120");
 				adView.setText("25");
 				defView.setText("4");
 				rarityView.setText("BESTEST");
+				image.setImageResource(R.drawable.creature4);
 				break;
 			default:
 				hpView.setText("ERROR");
